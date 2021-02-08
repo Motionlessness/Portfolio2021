d47a6ebcdb04aab784b6ba726d5abff935547f07
Wil Crouch <william.crouch@hotmail.com>
2021-02-08 10:34:14 -0800

    Fixed links and added controllers

---
+++ Controllers/ArtController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio2021.Controllers
+{
+    public class ArtController : Controller
+    {
+        private readonly ILogger<ArtController> _logger;
+
+        public ArtController(ILogger<ArtController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MinigamesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio2021.Controllers
+{
+    public class MinigamesController : Controller
+    {
+        private readonly ILogger<MinigamesController> _logger;
+
+        public MinigamesController(ILogger<MinigamesController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Minigame1()
+        {
+            return View();
+        }
+        public IActionResult Minigame2()
+        {
+            return View();
+        }
+
+    }
+}
